d0eb851c12f0b3e3368e67f25838d06c34293f47
Landon Turner <lturner@LTurner-MacBook-Pro.local>
2020-02-27 16:40:58 -0500

    added mapvalues to formikForm

--- user-onboarding-app/src/components/Form.js
+++ user-onboarding-app/src/components/Form.js
@@ -7,22 +7,37 @@ const Form = () =>{
     <div className='form-container'>
         <Form>
             <label htmlFor='name'>Name:
-                <Field id='name'/>
+                <Field id='name' name='name' type='text' placeholder='Name'/>
             </label>
             <label htmlFor='email'>Email:
-                <Field id='email'/>
+                <Field id='email' name='email' type='email' placeholder='Valid Email'/>
             </label>
             <label htmlFor='password'>Password:
-                <Field id='password'/>
+                <Field id='password' name='password' type='password' placeholder='Name'/>
             </label>
             <label htmlFor='tosCheck'>I agree with Terms of Service
-                <Field id='tosCheck'/>
+                <Field id='tosCheck' name='tosCheck' type='checkbox'/>
             </label>
             <button type='submit'>Submit</button>
         </Form>
     </div>
 }
 
-const formikForm = withFormik({})(Form);
+const formikForm = withFormik({
+    // mapPropsToValues: (props)=> {
+    //     return
+    // }
+    mapPropsToValues(props){
+        return {
+            name: props.name || '',
+            email: props.email || '', 
+            password: props.password || '',
+            tosCheck: props.tosCheck || false
+
+        }
+
+        
+    }
+})(Form);
 
 export default Form
